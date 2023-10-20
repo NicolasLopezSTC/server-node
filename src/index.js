f1efa6787d6b9c4ba70504e1d5994f1780115b17
NicolasLopezSTC <gclan149@gmail.com>
2023-10-20 02:20:52 -0500

    progress
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
-import './helpers/dotenv'
+import './helpers/config'
 
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
+import pino from 'pino'
 
-const express = require('express')
+
+
+import logger from './helpers/logger'
+import router from './routes'
+import { notFound, errorHandler } from './helpers/errors'
+
+
+const logger = pino({prettyPrint: { colorize: true}})
+
+const port = parseInt(process.env.PORT, 10) ||3000
 
 const app = express()
 
-const port = 3000
+app.use(morgan(process.env.MORGAN_LOG))
+app.use(cors({ origin: process.env.CORS_ORIGIN }))
+app.use(helmet())
 
-app.get('/', (req, res) => {
-  res.send({msg: 'process.env.TITLE' })
+app.get('/', (req, res)=> {
+  logger.info('Inside the root path')
+  const title = process.env.TITLE || 'Server'
+  res.send({ msg: title})
 })
 
-app.listen(port)
- 
+
+app.listen(port, () => 
+  logger.info(`Application started at http://localhost:${process.env.PORT}`)
+)
