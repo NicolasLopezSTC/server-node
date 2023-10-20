706ffea0a4b0c41d27abb9fd050a50bbe851588e
NicolasLopezSTC <gclan149@gmail.com>
2023-10-20 01:52:54 -0500

    Uh oh
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+import './helpers/dotenv'
+
+import express from 'express'
+
 const express = require('express')
+
 const app = express()
+
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send({msg: 'process.env.TITLE' })
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port)
+ 
